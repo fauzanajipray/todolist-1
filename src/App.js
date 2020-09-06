03097f689e292ab22e819a848e5aafd0ec193083
Fauzan Aji Prayoga <fauzanjr1@gmail.com>
2020-09-06 09:26:22 +0700

    Migrasi dari Pure ke React.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="container">
+        <div className="frame">
+          <section className="header">
+            <button className="header-btn main-black-color">Add</button>
+            <h1 className="header-title">todo list</h1>
+            <button className="header-btn main-red-color">Clear</button> 
+          </section>
+          <section className="add">
+            <form action="" className="add-form">
+              <input type="text" className="add-input" />
+              <button className="add-btn main-black-color">Add</button>
+            </form>
+          </section>
+          <section class="todos">
+            <div class="todo">
+                <span class="todo-text">Learning React</span>
+            </div>
+            <div class="todo">
+                <span class="todo-text">Learning React</span>
+            </div>
+            <div class="todo">
+                <span class="todo-text">Learning React</span>
+            </div>
+            <div class="todo">
+                <span class="todo-text">Learning React</span>
+            </div>
+            <div class="todo">
+                <span class="todo-text">Learning React</span>
+            </div>
+            <div class="todo">
+                <span class="todo-text">Learning React</span>
+            </div>
+            <div class="todo">
+                <span class="todo-text">Learning React</span>
+            </div>
+            <div class="todo">
+                <span class="todo-text">Learning React</span>
+            </div>
+            <div class="todo">
+                <span class="todo-text">Learning React</span>
+            </div>
+            <div class="todo">
+                <span class="todo-text">Learning React</span>
+            </div>
+          </section>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
