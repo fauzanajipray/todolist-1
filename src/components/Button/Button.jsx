6c4c189317c6d8767f6818075670d189f0db15bb
Fauzan Aji Prayoga <fauzanjr1@gmail.com>
2020-09-08 21:55:17 +0700

    + dependency classnames dan css modules

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styles from './button.module.css'
+import cx from 'classnames'
 
 const Button = ({ text, onClick, color, align }) =>{
 
-    const classNames = [
-        'header-btn',
-        color === "black" ? "main-black-color" : "main-red-color",
-        align === "left" ? "align-left" : "align-right"
-    ].join(" ")
-
-
+    // const classNames = [
+    //     'header-btn',
+    //     color === "black" ? "main-black-color" : "main-red-color",
+    //     align === "left" ? "align-left" : "align-right"
+    // ].join(" ")
+    const classNames = cx(styles.headerBtn, {
+        [styles.mainBlackColor]: color === 'black',
+        [styles.mainRedColor]: color === 'red',
+        [styles.alignLeft]: align === 'left',
+        [styles.alignRight]: align === 'right'
+    })
+    
     Button.defaultProps = {
         text: "Button",
         color: "black",
